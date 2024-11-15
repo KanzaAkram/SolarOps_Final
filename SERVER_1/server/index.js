dae854b75de1df7063c6b20bcec79f16f44a0f0a
daniya-sohail26 <119067734+daniya-sohail26@users.noreply.github.com>
2024-11-15 14:45:48 +0500

    changed arch

--- SERVER_1/server/index.js
+++ SERVER_1/server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require("morgan");
-const axios = require("axios");
 const port = 3500;
 const router = require("./Router/router");
 const app = express();
